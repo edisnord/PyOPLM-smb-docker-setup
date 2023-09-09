f92f2a54bee6e5508fe78fdb534e75758a24a7e4
edisnord <87529116+edisnord@users.noreply.github.com>
2023-09-09 13:43:59 +0200

    Removed aliases from install script

--- install.ps1
+++ install.ps1
@@ -1,3 +1,3 @@
-echo 'Adding command "pyoplm" to your terminal, make sure to have Docker Desktop in the background when running it'
+Write-Output 'Adding command "pyoplm" to your terminal, make sure to have Docker Desktop in the background when running it'
 Add-Content -Path $PROFILE -Value "New-Alias -Name pyoplm -Value $(Join-Path -Path $PSScriptRoot pyoplm.ps1);"
-echo 'Done! Restart your terminal and run "pyoplm" to test out the installation'
+Write-Output 'Done! Restart your terminal and run "pyoplm" to test out the installation'
